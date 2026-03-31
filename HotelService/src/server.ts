1b5870d90e44a741a93d4d5dcc1882ee3731c9bc
everestp <PAUDELEVEREST09@GMAIL.COM>
2026-03-31 22:42:40 +0545

    Add hotel models

--- HotelService/src/server.ts
+++ HotelService/src/server.ts
@@ -5,6 +5,7 @@ import v2Router from './routers/v2/index.router';
 import { appErrorHandler, genericErrorHandler } from './middlewares/error.middleware';
 import logger from './config/logger.config';
 import { attachCorrelationIdMiddleware } from './middlewares/correlation.middleware';
+import sequelize from './database/models/sequelize';
 const app = express();
 
 app.use(express.json());
@@ -15,7 +16,7 @@ app.use(express.json());
 
 app.use(attachCorrelationIdMiddleware);
 app.use('/api/v1', v1Router);
-app.use('/api/v2', v2Router); 
+app.use('/api/v2', v2Router);
 
 
 /**
@@ -26,7 +27,11 @@ app.use(appErrorHandler);
 app.use(genericErrorHandler);
 
 
-app.listen(serverConfig.PORT, () => {
+app.listen(serverConfig.PORT,  async () => {
     logger.info(`Server is running on http://localhost:${serverConfig.PORT}`);
     logger.info(`Press Ctrl+C to stop the server.`);
+
+await sequelize.authenticate();  //Test the connection to the database
+logger.info("Database connection has been established sucessfully")
+
 });
